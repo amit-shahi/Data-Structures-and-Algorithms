e5a07b29596df4c51c9c54d60be2beb541b429f0
Amit Shahi <ami.oneworld@gmail.com>
2019-06-02 03:20:26 +0530

    Iterative Generic binary search implementation for string & integer data structures
    Iterative Generic binary search implementation for string & integer data structures

---
+++ DataStructures/Iterative/BinarySearch/BinarySearch Iterative - v1/BinarySearch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch_Iterative___v1
+{
+    public class BinarySearch<T>
+    {
+        public int Search(T[] array, T target)
+        {
+            if (!String.IsNullOrEmpty(target.ToString()))
+
+                return SearchHelper(array, target, 0, array.Length - 1);
+
+            return -1;
+        }
+
+        private int SearchHelper(T[] array, T target, int left, int right)
+        {
+
+            //if (left > right)
+            //    return -1;
+
+            while (left <= right)
+            {
+
+                // NOTE: This will cause integer overflow issues
+                //int middle = (left + right) / 2;
+
+                // NOTE: To prevent interger overflow issues, use
+                int middle = left + ((right - left) / 2);
+
+                var currentItem = array[middle];
+
+
+
+                // Integer binary search implementation
+
+
+                //if (target == currentItem)
+                //    return middle;
+
+                //if (target < currentItem)
+                //    return SearchHelper(array, target, left, middle - 1);
+                //else
+                //    return SearchHelper(array, target, middle + 1, right);
+                //;
+
+                Comparer<T> comparer = Comparer<T>.Default;
+
+                // Generic binary search implementation for string & integer
+
+                // Check if target is present at middle position  
+
+                if (comparer.Compare(currentItem, target) == 0)
+                    return middle;
+
+                // Check if target is smaller then middle position, ignore right half   
+                else if (comparer.Compare(currentItem, target) > 0)
+                    
+                    right = middle - 1;
+                // Check if target is greater then middle position, ignore left half  
+                else
+
+                    left = middle + 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ DataStructures/Iterative/BinarySearch/BinarySearch Iterative - v1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using static System.Console;
+
+namespace BinarySearch_Iterative___v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Binary Search for an item in a sorted array list.");
+            WriteLine();
+
+            WriteLine("1. Perform a Iterative Binary Search in a sorted list of integers.");
+
+            int[] integerList = new int[] { 1, 13, 40, 2, 5, 25, 44, 9, -1, 7, 6 };
+
+            BinarySearch<int> _integerBinarySearch = new BinarySearch<int>();
+
+            WriteLine();
+            WriteLine("Given Array List");
+            WriteLine(String.Join(" ", integerList));
+
+            // Make sure to sort the given array of integers to make sure they are sorted.
+            Array.Sort(integerList);
+
+            int lookupInteger = _integerBinarySearch.Search(integerList, 25);
+
+
+
+            WriteLine();
+            WriteLine("Sorted Array List");
+            WriteLine(String.Join(" ", integerList));
+
+            //int index = 0;
+            //foreach (int number in integerList)
+            //{
+            //    //WriteLine("[" + index + "] => " + number + " ");
+            //    index++;
+            //}
+
+            WriteLine();
+            WriteLine();
+
+            if (lookupInteger != -1)
+            {
+                WriteLine($"Found {25} in the list at the position {lookupInteger}");
+            }
+            else
+            {
+                WriteLine("Item not found");
+            }
+
+            WriteLine();
+            WriteLine("***********************************************************************************");
+            WriteLine();
+            WriteLine("2. Perform a Iterative Binary Search in a sorted list of strings.");
+
+            string[] stringList = new string[] { "One", "Apple", "Orange", "Mango", "Zebra", "Yummy" };
+
+            BinarySearch<string> _stringBinarySearch = new BinarySearch<string>();
+
+            WriteLine();
+            WriteLine("Given Array List");
+            WriteLine(String.Join(" ", stringList));
+
+            // Make sure to sort the given array of integers to make sure they are sorted.
+            Array.Sort(stringList);
+
+            int lookupString = _stringBinarySearch.Search(stringList, "Mango");
+
+
+
+            WriteLine();
+            WriteLine("Sorted Array List");
+            WriteLine(String.Join(" ", stringList));
+
+            //int index = 0;
+            //foreach (int number in integerList)
+            //{
+            //    //WriteLine("[" + index + "] => " + number + " ");
+            //    index++;
+            //}
+
+            WriteLine();
+             
+
+            if (lookupString != -1)
+            {
+                WriteLine($"Found Mango in the list at the position {lookupString}");
+            }
+            else
+            {
+                WriteLine("Item Mango not found");
+            }
+
+
+        }
+    }
+}
