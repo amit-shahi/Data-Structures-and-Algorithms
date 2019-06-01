bfde7a605f562b1425d8419ab4f88517c78f7dda
Amit Shahi <ami.oneworld@gmail.com>
2019-06-01 23:59:27 +0530

    Added Recursive Implementation of Binary Search
    Added Recursive Implementation of Binary Search

---
+++ DataStructures/Recursive/BinarySearch/BinarySearch - v1/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch___v1
+{
+    public class BinarySearch<T>
+    {
+        public int Search(int[] array, int target)
+        {
+            if(target != 0 )
+            return SearchHelper(array, target, 0, array.Length-1);
+
+            return -1;
+        }
+
+        public int SearchHelper(int[] array, int target, int left, int right)
+        {
+            if (left > right)
+                return -1;
+
+            int middle = (left + right) / 2;
+            int currentItem = array[middle];
+
+            if ( target == currentItem)
+                return middle;
+
+            if ( target < currentItem )
+                return SearchHelper(array, target, left, middle - 1);
+            else
+                return SearchHelper(array, target, middle + 1, right);
+        }
+    }
+
+   
+}
--- DataStructures/Recursive/BinarySearch/BinarySearch - v1/Program.cs
+++ DataStructures/Recursive/BinarySearch/BinarySearch - v1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+
 
 namespace BinarySearch___v1
 {
@@ -6,7 +8,35 @@ namespace BinarySearch___v1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Binary Search for an item in a sorted array list.");
+            WriteLine("Binary Search for an item in a sorted array list.");
+            WriteLine();
+
+            WriteLine("1. Perform a Binary Search in a sorted list of integers.");
+
+            int[] integerList = new int[] { 1, 13, 40, 2, 5, 25, 44, 9, -1, 7, 6 };
+
+            BinarySearch<int> _binarySearch = new BinarySearch<int>();
+
+            // Make sure to sort the given array of integers to make sure they are sorted.
+            Array.Sort(integerList);
+
+            int lookupInteger = _binarySearch.Search(integerList, 25);
+
+            foreach(int number in integerList)
+            {
+                Write(number + " ");
+            }
+
+            if(lookupInteger != -1)
+            {
+                WriteLine($"Found {25} in the list at {lookupInteger}");
+            }
+            else
+            {
+                WriteLine("Item not found");
+            }
+            
+
         }
     }
 }
