6f04d81e88405113117a115e1abc92e537cd3d6e
Amit Shahi <ami.oneworld@gmail.com>
2019-07-14 02:32:07 +0530

    Updated  followings for Doubly Linked List
    * SetHead() * SetTail()
    * PeekHeadOrFirst()
    * PeekTailOrLast()
    * RemoveHeadOrFirst()
    * RemoveTailOrLast()
    * PrintList()
    * HeadandTail()

--- DataStructures/Iterative/Doubly Linked List/Program.cs
+++ DataStructures/Iterative/Doubly Linked List/Program.cs
@@ -1,100 +1,199 @@
 ﻿using System;
 using static System.Console;
+using System.Text;
 
 namespace Doubly_Linked_List
 {
-    public class Node
-    {
-        public int data;
-
-        public Node prev;
-        public Node next;
-
-        public Node(int data)
-        {
-            this.data = data;
-            prev = null;
-            next = null;
-        }
-    }
-
+    
     public class DoublyLinkedList
     {
-        public Node head;
-        public Node tail;
+        // Internal node class to represent data
+        private class Node
+        {
+            internal int data;
+
+            internal Node prev, next;
+            public Node(int data, Node prev, Node next)
+            {
+                this.data = data;
+                this.prev = prev;
+                this.next = next;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+ 
+        public DoublyLinkedList()
+        {
+            head = null;
+            tail = null;
+        }
 
 
-        public void setHead(Node _node)
+        public void SetHead(int data)
         {            
             if(head == null)
             {
-                Node node = new Node(_node.data);
-
-                head = node;
-                tail = node;
-               // node.next = head;
-                return;
+                Node newNode = new Node(data, null, null);
+                //Point newly created node as head & tail if the list contain only 1 item
+                head = tail = newNode;
             }
-
-            // call insertBefore()
+            else 
+            {
+                Node newHead = new Node(data,null, head);
+                // Correcting prev pointer of head 
+                head.prev = newHead;
+                head = head.prev; 
+            }
+ 
         }
 
-        public void setTail(Node node)
+        public void SetTail(int data)
         {
             if(tail == null)
             {
-                setHead(node);
-                return;
+                SetHead(data);
+            }
+            else 
+            {
+                Node newTail = new Node(data, tail.prev, null);
+                // Update next pointer of current Tail to point to this newTail
+                tail.next = newTail;
+                tail  = tail.next;
             }
             // insertAfter()
         }
 
-        public void insertBefore(Node node, Node newNode)
+        // Check the value of the First Node if it exists - O(1) time
+        public int PeekHeadOrFirst()
         {
+           if(head == null) throw new Exception("List is empty");
 
+           return head.data; 
         }
 
-        public void insertAfter(Node node, Node newNode)
+        // Check the value of the Last Node if it exists - O(1) time
+        public int PeekTailOrLast()
         {
+           if(tail == null) throw new Exception("List is empty");
 
+           return tail.data; 
         }
 
-        public void insertAtPosition(Node node, int position, Node newNode)
+        // Remove the first value at the head of linked list - O(1) time
+        public int RemoveHeadOrFirst()
         {
+            if(head == null) throw new Exception("List is empty");
+            
+            int tempData = head.data;
+            head = head.next;
 
+            // Check to make sure if the list is empty after removing the current Head, set tail also as empty.
+            if(head == null)
+                tail = null;
+            else
+               // Clean memory of the previous node
+                head.prev = null;
+
+            return tempData;
         }
 
-        public void removeNode(Node node)
+        // Remove the last value at the tail of linked list - O(1) time
+        public int RemoveTailOrLast()
         {
+            // if(tail == null) throw new Exception("List is empty");
+            
+            // int tempData = tail.data;
+            // tail = tail.prev;
 
+            // // Check to make sure if the list is empty after removing the current Tail, set head also as empty.
+            // // if(head == null)
+            // //    head = null;
+            // // else
+            // //    //Clean memory of the previous node
+            // //    tail.next = null;
+
+            // return tempData;
+
+            if(head == null) throw new Exception("List is empty");
+            if(head.next == null) throw new Exception("Next Node is empty");
+
+            Node lastNode = tail;
+            // Find the second last node 
+            Node second_last = head;
+            
+
+            while (second_last.next.next != null)
+            { 
+                second_last = second_last.next; 
+            }
+  
+             // Update the next pointer of second last to null
+            second_last.next = null;
+            tail =  second_last;
+        
+            return lastNode.data;
         }
 
-        public void removeNodeAtPosition(Node node, int position)
-        {
 
-        }
+        // public void insertBefore(Node node, Node newNode)
+        // {
 
-        public void removeNodesWithValue(int value)
-        {
+        // }
 
-        }
+        // public void insertAfter(Node node, Node newNode)
+        // {
 
-        public void containsNodewithValue(Node node)
-        {
+        // }
 
-        }
+        // public void insertAtPosition(Node node, int position, Node newNode)
+        // {
 
-        public void printList(Node head)
+        // }
+
+        // public void removeNode(Node node)
+        // {
+
+        // }
+
+        // public void removeNodeAtPosition(Node node, int position)
+        // {
+
+        // }
+
+        // public void removeNodesWithValue(int value)
+        // {
+
+        // }
+
+        // public void containsNodewithValue(Node node)
+        // {
+
+        // }
+
+        public void PrintList()
         {
             Node node = head;
 
             while(node != null)
             {
-                Write($" {node.data} -> ");
+                Write($" {node.data} <=> ");
                 node = node.next;
             }
-            Write("NULL");
 
+            this.HeadandTail();
+        }
+
+        public void HeadandTail()
+        {
+           WriteLine();
+           WriteLine("-----------------------------------------");
+
+           Write($"Current Head: {this.PeekHeadOrFirst()} & Current Tail: {this.PeekTailOrLast()}");
+           
+           WriteLine();
+           WriteLine("-----------------------------------------");
         }
     }
     public class Program
@@ -114,22 +213,55 @@ namespace Doubly_Linked_List
             // Removing Two or More Item
             // O(n) time | O(1) time
 
-            // setHead(), setTail(), insertBefore(), insertAfter()
+            // SetHead(), SetTail(), insertBefore(), insertAfter()
             // O(1) time | O(1) space
 
             // insertAtPosition(), 
             // O(p) time -- Because we have to traverse all the way to the position |
             // O(1) space
-            Node head = null;
            // head.next = new Node(200);
-             
+            
+            WriteLine();
+
             DoublyLinkedList dllist = new DoublyLinkedList();
 
-           dllist.setHead(new Node(500));
-          // dllist.setTail(new Node(20));
-           dllist.printList(head); 
+           dllist.SetHead(500);
+           //
+           dllist.SetHead(200);
+           dllist.SetTail(700);
+           dllist.SetTail(50);
+           dllist.SetHead(600);
 
-            
+           dllist.PrintList(); 
+           WriteLine();
+ 
+           WriteLine($"Removing Head: {dllist.RemoveHeadOrFirst()}");
+           WriteLine();
+
+           dllist.PrintList(); 
+           WriteLine();
+ 
+           WriteLine($"Removing Tail: {dllist.RemoveTailOrLast()}");
+           WriteLine();
+
+           dllist.PrintList(); 
+           WriteLine();
+
+           WriteLine($"Removing Head: {dllist.RemoveHeadOrFirst()}");
+           WriteLine();
+
+           dllist.PrintList(); 
+           WriteLine();
+ 
+           WriteLine($"Removing Tail: {dllist.RemoveTailOrLast()}");
+           WriteLine();
+
+           dllist.PrintList(); 
+           WriteLine();
+
+
+           WriteLine();
+ 
         }
     }
 }
